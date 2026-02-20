988cbb379d8fb58c34fea380e2bc63e4611102b4
Salwa-pratama <nandev1915@gmail.com>
2026-02-20 22:06:29 +0700

    update dikit

--- src/components/views/templates/Header.tsx
+++ src/components/views/templates/Header.tsx
@@ -3,12 +3,18 @@ import Logo from "../../../assets/react.svg";
 export default function Header() {
   return (
     <div
-      className="flex w-full bg-stone-800 text-white p-5 font-bold "
+      className="flex w-full bg-stone-800 text-white p-5 font-bold justify-items-center"
       id="header"
     >
       <div className="flex items-center gap-4 ">
         <h1 className="text-2xl ">Learn React </h1>
         <img src={Logo} alt="" className="h-10" />
+      </div>
+      <div className="ml-auto flex flex-col justify-self-end">
+        <h1 className="">Create by ananda salwa pratama</h1>
+        <p className="ml-auto font-thin text-sm">
+          Last update : 20, february 2026
+        </p>
       </div>
     </div>
   );
--- src/components/views/templates/Layout.tsx
+++ src/components/views/templates/Layout.tsx
@@ -5,17 +5,22 @@ import Header from "./Header";
 import Footer from "./Footer";
 
 export default function Layout() {
+  const path = window.location.pathname;
+
   return (
     <div>
       {/* Header */}
       <Header />
 
-      <div className="">
-        <a href="/" className="w-full bg-primary p-4 block">
-          Back Home
-        </a>
-      </div>
-
+      {path === "/" ? (
+        <div></div>
+      ) : (
+        <div className="">
+          <a href="/" className="w-full bg-primary p-4 block">
+            Back Home
+          </a>
+        </div>
+      )}
       <main className="border scrollbar-hide scroll-smooth overflow-auto h-[900px] p-10 border-black m-5 mx-auto ">
         <Outlet />
       </main>
